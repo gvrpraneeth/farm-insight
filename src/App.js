6feff450bb10668cfd5785a7f60cb54a9409a8eb
venkatar.gurramkondu <venkatar.gurramkondu@servicenow.com>
2023-12-20 02:13:59 +0530

    added background image, logo and buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import background from './farming.jpg'
+
 import './App.css';
 
 function App() {
   return (
-    <div style={{ backgroundImage: `url(${background})`}}>
+    <div className = 'main-container'>
+       
+        <div style={{ height:"100vh", backgroundImage:"url(/farming.jpg)", backgroundSize: "cover",
+               backgroundRepeat: "no-repeat",}}>
+
+        <div className='logo'> Farm Insight</div>
+
+
+
+<div className='buttons-container'>
+<div className='buttons' >
+          <button className='button' >Create survey</button>
+
+          
+        </div>
+       
+        <div className='buttons' >
+          
+
+          <button className='button' >View survey results</button>
+        </div>
+         
+        </div>
+</div>
         
-  </div>
+
+        
+      
+
+    </div>
+   
   );
 }
 
